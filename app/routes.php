cb5b8b9e9acdc16d66b7f27a557d2a07b494f369
omegaalfa <omegaalfa.wr3@gmail.com>
2017-07-09 15:46:29 -0300

    Adiciona helpers e views renderes

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,6 @@ use Lib\http\Request;
 use Core\Router;
 use App\Controller;
 
-//require_once 'controllers/HomeController.php';
 
 $request = new Request();
 $app = new Router($request);
--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,6 @@
 <?php
 
-require_once  __DIR__.'/../core/bootstrap.php';
+session_start();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../core/bootstrap.php';
