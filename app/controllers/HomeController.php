33c03356d471b07aa76c964927d07d12d062a03e
omegaalfa <omegaalfa.wr3@gmail.com>
2017-11-02 00:39:55 -0200

    view

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
     public function index()
     {
         $data = array('nome' =>'Fulano');
-        $this->renderView('login', $data);
+        $this->renderView('home', $data);
 
     }
 
